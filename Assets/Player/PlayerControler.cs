1d4d41c69f1b5ead75e9a58504f1b3c219c48334
AttackDorito <75434307+AttackDorito@users.noreply.github.com>
2025-01-24 22:18:45 +0000

    inputsystem player movement

--- Assets/Player/PlayerControler.cs
+++ Assets/Player/PlayerControler.cs
@@ -1,22 +1,27 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerControler : MonoBehaviour
 {
     private Rigidbody2D rb;
+
+    public float moveForce = 15f;
+    public float maxSpeed = 15f;
     
-    public float verticalSpeed = 10;
-    public float horizontalSpeed = 10;
+    InputAction moveAction;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        moveAction = InputSystem.actions.FindAction("Move");
     }
 
     // Update is called once per frame
     void FixedUpdate()
-    {
-        float horizontal = Input.GetAxis("Horizontal")*horizontalSpeed;
-        float vertical = Input.GetAxis("Vertical")*verticalSpeed;
-        rb.linearVelocity = new Vector2(horizontal, vertical);
+    {   
+        Vector2 inputVector = moveAction.ReadValue<Vector2>();
+        rb.AddForce(inputVector * moveForce);
+        rb.linearVelocity = Vector2.ClampMagnitude(rb.linearVelocity, maxSpeed);
     }
 }
